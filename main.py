ed1ee99e0c8282a5fc5fcc1aa086411090defaa2
simongeek <szyplotk@student.pg.gda.pl>
2017-05-29 00:30:24 +0200

    zmiana3

--- main.py
+++ main.py
@@ -4,45 +4,31 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 from datetime import date, datetime, time
 import time
-
+import seaborn as sns
 
 #Import USA Weather data from CSV file
 data = pd.read_csv('weather.csv')
 
 #Make variables some friendlier names for users
-"""data['PDT'] = data['date']
-data['Max TemperatureF'] = data['maxTemp']
-data['Min TemperatueF'] = data['minTemp']
-data['Mean TemperatureF'] = data['meanTemp']
-data['Max Dew PointF'] = data['maxDew']
-data['MeanDew PointF'] = data['meanDew']
-data['Min DewpointF'] = data['minDew']
-data['Max Humidity'] = data['maxHum']
-data['Mean Humidity'] = data['meanHum']
-data['Max Sea Level PressureIn'] = data['maxPress']
-data['Mean Sea Level PressureIn'] = data['minPress']
-data['Min Sea Level PressureIn'] = data['meanPress']
-data['Max VisibilityMiles'] = data['maxVis']
-data['Mean VisibilityMiles'] = data['meanVis']
-data['Min VisibilityMiles'] = data['minVis']
-data['Max Wind SpeedMPH'] = data['maxWind']
-data['Mean Wind SpeedMPH'] = data['meanWind']
-data['Max Gust SpeedMPH'] = data['maxGust']
-data['PrecipitationIn'] = data['preIn']
-data['CloudCover'] = data['cloud']
-data['WindDirDegrees'] = data['WindDir']
-"""
-#Convert Date Format
+old_names = ['Max TemperatureF', 'Min TemperatureF', 'Mean TemperatureF','Max Dew PointF','MeanDew PointF','Min DewpointF','Max Humidity',
+             'Mean Humidity','Max Sea Level PressureIn','Mean Sea Level PressureIn','Min Sea Level PressureIn', 'Max VisibilityMiles','Mean VisibilityMiles',
+             'Min VisibilityMiles','Max Wind SpeedMPH', 'Mean Wind SpeedMPH','Max Gust SpeedMPH','PrecipitationIn', 'CloudCover','WindDirDegrees' ]
+new_names = ['maxTemp', 'minTemp', 'meanTemp','maxDew','meanDew','minDew','maxHum','meanHum','maxPress','minPress','meanPress','maxVis','meanVis',
+             'minVis','maxWind','meanWind','maxGust','preIn','cloud','WindDir']
+data.rename(columns=dict(zip(old_names, new_names)), inplace=True)
+
+#Get data for first year
+
 
 
 
 #Remove the bad samples
-data = data[(data['Max TemperatureF'] < 110) & (data['Min TemperatureF'] > 25)]
+data = data[(data['maxTemp'] < 110) & (data['minTemp'] > 25)]
 
 #Plots of min, max and mean temperature in Fahrenheit scale
 
 plt.figure()
-df1 = pd.DataFrame(data, columns=['Max TemperatureF', 'Min TemperatureF', 'Mean TemperatureF'])
+df1 = pd.DataFrame(data, columns=['maxTemp', 'minTemp', 'meanTemp'])
 plt.plot(df1, '-')
 plt.axis([0,1850, 0, 140])
 plt.grid(True)
@@ -57,7 +43,4 @@ df2 = pd.DataFrame(data, columns=['ZIP'])
 u = df2.drop_duplicates(['ZIP'])
 print(u)
 
-#Replace ZIP Code to City name
-
-
-#
+print(data['preIn'])
